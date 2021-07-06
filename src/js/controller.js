940d552693bec1274e804c23d5959bbd032de5ee
erikellilo <erikellilo@gmail.com>
2021-07-06 17:23:36 +0700

    added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("welcome to the application ");
+};
+
 const init = function () {
   bookmarkView.addHandlerRenderBookmark(controlBookmark);
   // !subs-publish pattern
@@ -135,5 +139,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
